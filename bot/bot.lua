3fa64819eda12435b1be968b5be2eaa7995bdbfc
elixboyBW <apkstars.ir@gmail.com>
2016-02-06 16:50:00 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {161762902},
+    sudo_users = {user 161762902 id},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
